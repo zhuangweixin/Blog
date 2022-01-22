61fb59d502c4283884a05b2a6fcff59af69741ec
zhuangweixin <995245717@qq.com>
2022-01-22 11:51:19 +0800

    ceshi

--- FilePush.bat
+++ FilePush.bat
@@ -1,11 +1,9 @@
 @echo off
 echo "-------Begin-------"
-git status
 set /p msg=请输入提交注释:
 git add .
 git commit -m %msg%
-git pull
-git push
+git push origin
 echo 推送成功：【%msg%】
 echo "--------End!--------"
 pause
